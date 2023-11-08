9be03ee43b503b5113817db7e4fb08098d455f8d
JakubDrzala <jakubdrzalaptn@gmail.com>
2023-11-08 12:30:15 +0100

    stworzenie projektu

---
+++ PlanLekcji/src/main/java/com/mycompany/planlekcji/PlanLekcji.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.planlekcji;
+
+/**
+ *
+ * @author admin
+ */
+public class PlanLekcji {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
